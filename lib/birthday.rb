0d76179220feea5132e7e2fc81a76109eb2f31f0
your_name <your_email@example.com>
2020-10-29 18:30:22 -0400

    commit tree 8a877e480e98a64d40d7fb83cb3dda5bcafd16bd

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,13 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-end
+  birthday_kids.each do |key, value|
+    #  if value < 12
+      
+      puts "Happy Birthday #{key}! You are now #{value} years old!"
+     
+    end 
+  end 
+
 
 
